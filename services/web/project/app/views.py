844bc9183cc0031c5bb12a55723f869dfb5a71d2
Philippe Carvajal <phcp1418@gmail.com>
2021-10-16 14:40:44 -0400

    [#26] New imports + new_question method
    • Imported redirect, and url_for from flask
    • Imported flash from flask.helpers
    • Created 'controller' logic and routing for new_question

--- services/web/project/app/views.py
+++ services/web/project/app/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template
-from flask.helpers import send_from_directory
+from flask import Blueprint, render_template, redirect, url_for
+from flask.helpers import send_from_directory, flash
 from flask_login import login_required, current_user
 from project.app.models import *
+from project.app.forms import NewQuestionForm
 
 views = Blueprint('views', __name__)
 
@@ -20,6 +21,26 @@ def questions():
 		q.creator = User.query.get(q.userId)
 		q.numAnswers = len(Answer.query.filter_by(questionId = Answer.questionId).all())
 	return render_template("questions.html", user=current_user, questions_list=questions)
+
+@views.route('/new_question', methods=['GET','POST'])
+@login_required
+def new_question():
+	form = NewQuestionForm()
+	if form.validate_on_submit():
+		title = form.title.data
+		body = form.body.data
+		# Add new question in DB
+		q = Question(title,body,current_user.id)
+		db.session.add(q)
+		db.session.commit()
+		return question(q)
+
+	return render_template("new_question.html", form=form)
+
+@views.route('/question')
+@login_required
+def question(question):
+	return question.title
 
 # Temporary method returns files in static/css/
 @views.route('/static/css/<filename>')
