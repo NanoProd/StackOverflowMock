4016c5792c568ada64ff25aee428d76742352352
Philippe Carvajal <phcp1418@gmail.com>
2021-10-29 20:44:22 -0400

    [#71] Refactored + Added feature code
    Refactoring:
    • staticfile() was refactored to allow for url_for() to be called as
    follows: url_for('views.staticfile', folder='<foldername>', filename='<filename>'). This resolves the issue of this method not returning files properly and not returning files from the js folder at all. Note that the js folder was added back to the file structure.
    • Imported send_file
    • Refactored views.question to function as a controller where a method
    and one or more arguments can be passed. This decouples the method from
    a specific functionality implemented within the "controller" and allows
    for a modular approach to developing new functionalities within the
    "controller."
    
    New Feature:
    • Implemented the accepted answer feature in views,question.
    • Modified the logic that gets a new question to account for the new
    feature in processing the list of answers and the fron-end behaviour.
    • Added TODOS for further improvement.

--- services/web/project/app/views.py
+++ services/web/project/app/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask.helpers import send_from_directory
+from flask.helpers import send_from_directory, send_file
 
 from flask_login import login_required, current_user
 from project import db
@@ -35,39 +35,88 @@ def new_question():
         q = Question(title, body, current_user.id)
         db.session.add(q)
         db.session.commit()
-        return redirect(url_for("views.question", question_id=q.id))
+        return redirect(url_for("views.question", method="getQuestion", arg1=q.id))
 
     return render_template("new_question.html", form=form)
 
+@views.route('/questions/', defaults={'method':'None', 'arg1':'None', 'arg2':'None'})
+@views.route('/questions/<method>/', defaults={'arg1':'None', 'arg2':'None'})
+@views.route('/questions/<method>/<arg1>', defaults={'arg2':'None'}, methods=['GET', 'POST'])
+@views.route('/questions/<method>/<arg1>/<arg2>', methods=['GET', 'POST'])
+def question(method,arg1,arg2):
+    # Make best answer method
+    if(method == "acceptAnswer"):
+        answer_id = arg1
+        question_id = arg2
+        # Get all answers for question; filter by is_accepted_answer = true
+        accepted_answer = Answer.query.filter_by(is_accepted_answer=True,questionId=question_id).all()
+        # If question does not yet have a best answer then
+        if(len(accepted_answer) < 1):
+            # Get answer from DB
+            answer = Answer.query.get(answer_id)
+            #  If answer could not be found
+            if(answer == None):
+                # Return error message to user
+                # TODO: Log error to file instead.
+                return "ANSWER_NOT_FOUND_ERROR"
 
-@views.route('/questions/<question_id>', methods=['GET', 'POST'])
-def question(question_id):
-    # Get question from DB
-    question = Question.query.get(question_id)
-    # Validate that question exists; if not, route to questions forum
-    if question is None:
-        questions()
-    # Get list of answers for question
-    question.answers = Answer.query.filter_by(
-        questionId=question.id).order_by(Answer.numVotes.desc()).all()
-    question.numAnswers = len(question.answers)
-    # Get the question's creator and assign it as an attribute
-    question.creator = User.query.get(question.userId)
+            # TODO: If user is not owner of question, log error.
 
-    # Process form
-    form = NewAnswerForm()
-    if form.validate_on_submit():
-        body = form.body.data
-        # Add answer to DB
-        a = Answer(body, current_user.id, question.id)
-        db.session.add(a)
-        db.session.commit()
-        return redirect(url_for('views.question', question_id=question_id))
+            # Otherwise update is_accepted_answer column of answer to true
+            answer.is_accepted_answer = True
+            db.session.commit()
+            # Reload question so that accepted answer appears at top of the list.
+            return redirect(url_for("views.question", method="getQuestion", arg1=question_id))
+        # If question already has a best answer the
+        else:
+            # Return error message to user
+            # TODO: Log error to file instead.
+            return "ACCEPTED_ANSWER_EXISTS"
+    # Get Queston method
+    elif(method == "getQuestion"):
+        # Get question from DB
+        question = Question.query.get(arg1)
+        # Validate that question exists; if not, route to questions forum
+        if question is None:
+            questions()
+        # Get list of answers for question
+        question.answers = Answer.query.filter_by(questionId=question.id).order_by(Answer.numVotes.desc()).all()
+        question.numAnswers = len(question.answers)
+        question.hasAcceptedAnswer = False # Default value, condition is checked below
+        question.user_is_owner = False # Default value, condition is checked below
+        # Get the question's creator and assign it as an attribute
+        question.creator = User.query.get(question.userId)
 
-    # For each answer, add the creator as an attribute
-    for a in question.answers:
-        a.creator = User.query.get(a.userId)
-    return render_template('question.html', question=question, form=form)
+        # Place accepted answer at the top of the list
+        for a in question.answers:
+            if(a.is_accepted_answer == True):
+                # Remove answer from list.
+                question.answers.remove(a)
+                # Prepend answer to list.
+                question.answers.insert(0,a)
+                # Indicate that question has accepted answer
+                question.hasAcceptedAnswer = True
+
+        # Determine if user owns question
+        if (current_user.id == question.userId):
+            question.user_is_owner = True
+
+        # Process form
+        form = NewAnswerForm()
+        if form.validate_on_submit():
+            body = form.body.data
+            # Add answer to DB
+            a = Answer(body, current_user.id, question.id)
+            db.session.add(a)
+            db.session.commit()
+            return redirect(url_for('views.question', method="getQuestion", arg1=question.id))
+
+        # For each answer, add the creator as an attribute
+        for a in question.answers:
+            a.creator = User.query.get(a.userId)
+        return render_template('question.html', question=question, form=form)
+    else:
+        return questions()
 
 
 @views.route('/vote/<question_id>/<answer_id>/<value>', methods=['GET'])
@@ -84,7 +133,7 @@ def vote(question_id, answer_id, value):
     return redirect(request.referrer)
 
 # Temporary method returns files in static/*/
-@views.route('/static/<foldername>/<filename>')
-def staticfile(foldername,filename):
-    path_to_file = foldername + "/" + filename;
-    return send_from_directory('app/static/', path_to_file)
+@views.route('/static/<folder>/<filename>')
+def staticfile(folder,filename):
+    path = 'app/static/' + folder + '/' + filename
+    return send_file(path)
