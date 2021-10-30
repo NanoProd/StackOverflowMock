c582d8abbd4c51cab638d55302f4ea2cbcbc5863
Philippe Carvajal <phcp1418@gmail.com>
2021-10-30 15:39:37 -0400

    [#48] Refactored to adhere to flake8 standard

--- services/web/project/app/controllers.py
+++ services/web/project/app/controllers.py
@@ -1,98 +1,101 @@
-from flask import Blueprint, render_template, redirect, url_for, request
-from flask.helpers import send_file
-
-from flask_login import login_required, current_user
+from flask_login import current_user
 from project import db
 from project.app.models import User, Question, Answer
-from project.app.forms import NewQuestionForm, NewAnswerForm
+from project.app.forms import NewAnswerForm
+
 
 class QuestionCtrl():
 
-	def getQuestion(question_id):
-		result = list()
-		# Get question from DB
-		question = Question.query.get(question_id)
-		
-		# Validate that question exists; if not, route to questions forum
-		if question is None:
-			result.append('REDIRECT_1')
-			result.append('views.questions')
-			return result
-		
-		# Get list of answers for question
-		question.answers = Answer.query.filter_by(questionId=question.id).order_by(Answer.numVotes.desc()).all()
-		question.numAnswers = len(question.answers)
-		question.hasAcceptedAnswer = False  # Default value, condition is checked below
-		question.user_is_owner = False  # Default value, condition is checked below
-		# Get the question's creator and assign it as an attribute
-		question.creator = User.query.get(question.userId)
+    def getQuestion(question_id):
+        result = list()
+        # Get question from DB
+        question = Question.query.get(question_id)
 
-		# Place accepted answer at the top of the list
-		for a in question.answers:
-			if(a.is_accepted_answer is True):
-				# Remove answer from list.
-				question.answers.remove(a)
-				# Prepend answer to list.
-				question.answers.insert(0, a)
-				# Indicate that question has accepted answer
-				question.hasAcceptedAnswer = True
+        # Validate that question exists; if not, route to questions forum
+        if question is None:
+            result.append('REDIRECT_1')
+            result.append('views.questions')
+            return result
 
-		# Determine if user owns question
-		if (current_user.id == question.userId):
-			question.user_is_owner = True
+        # Get list of answers for question
+        question.answers = Answer.query.filter_by(
+            questionId=question.id).order_by(Answer.numVotes.desc()).all()
+        question.numAnswers = len(question.answers)
+        # Default value, condition is checked below
+        question.hasAcceptedAnswer = False
+        # Default value, condition is checked below
+        question.user_is_owner = False
+        # Get the question's creator and assign it as an attribute
+        question.creator = User.query.get(question.userId)
 
-		# Process form
-		form = NewAnswerForm()
-		if form.validate_on_submit():
-			body = form.body.data
-			# Add answer to DB
-			a = Answer(body, current_user.id, question.id)
-			db.session.add(a)
-			db.session.commit()
+        # Place accepted answer at the top of the list
+        for a in question.answers:
+            if(a.is_accepted_answer is True):
+                # Remove answer from list.
+                question.answers.remove(a)
+                # Prepend answer to list.
+                question.answers.insert(0, a)
+                # Indicate that question has accepted answer
+                question.hasAcceptedAnswer = True
 
-			result.append("REDIRECT_2")
-			result.append("views.question")
-			result.append("getQuestion")
-			result.append(question_id)
-			return result
+        # Determine if user owns question
+        if (current_user.id == question.userId):
+            question.user_is_owner = True
 
-		# For each answer, add the creator as an attribute
-		for a in question.answers:
-			a.creator = User.query.get(a.userId)
+        # Process form
+        form = NewAnswerForm()
+        if form.validate_on_submit():
+            body = form.body.data
+            # Add answer to DB
+            a = Answer(body, current_user.id, question.id)
+            db.session.add(a)
+            db.session.commit()
 
-		result.append("RENDER_TEMPLATE")
-		result.append('question.html')
-		result.append(question)
-		result.append(form)
-		return result
+            result.append("REDIRECT_2")
+            result.append("views.question")
+            result.append("getQuestion")
+            result.append(question_id)
+            return result
 
-	def acceptAnswer(answer_id, question_id):
-		result = list()
-		# Get all answers for question; filter by is_accepted_answer = true
-		accepted_answer = Answer.query.filter_by(is_accepted_answer=True, questionId=question_id).all()
-		# If question does not yet have a best answer then
-		if(len(accepted_answer) < 1):
-			# Get answer from DB
-			answer = Answer.query.get(answer_id)
-			#  If answer could not be found
-			if(answer is None):
-				# Return error message to user
-				# TODO: Log error to file instead.
-				return "ANSWER_NOT_FOUND_ERROR"
+        # For each answer, add the creator as an attribute
+        for a in question.answers:
+            a.creator = User.query.get(a.userId)
 
-			# TODO: If user is not owner of question, log error.
+        result.append("RENDER_TEMPLATE")
+        result.append('question.html')
+        result.append(question)
+        result.append(form)
+        return result
 
-			# Otherwise update is_accepted_answer column of answer to true
-			answer.is_accepted_answer = True
-			db.session.commit()
-			# Reload question so that accepted answer appears at top of the list.
-			result.append("REDIRECT")
-			result.append("views.question")
-			result.append("getQuestion")
-			result.append(question_id)
-			return result
-		# If question already has a best answer the
-		else:
-			# Return error message to user
-			# TODO: Log error to file instead.
-			return "ACCEPTED_ANSWER_EXISTS"
+    def acceptAnswer(answer_id, question_id):
+        result = list()
+        # Get all answers for question; filter by is_accepted_answer = true
+        accepted_answer = Answer.query.filter_by(
+            is_accepted_answer=True, questionId=question_id).all()
+        # If question does not yet have a best answer then
+        if(len(accepted_answer) < 1):
+            # Get answer from DB
+            answer = Answer.query.get(answer_id)
+            #  If answer could not be found
+            if(answer is None):
+                # Return error message to user
+                # TODO: Log error to file instead.
+                return "ANSWER_NOT_FOUND_ERROR"
+
+            # TODO: If user is not owner of question, log error.
+
+            # Otherwise update is_accepted_answer column of answer to true
+            answer.is_accepted_answer = True
+            db.session.commit()
+            # Reload question so that accepted answer
+            # appears at top of the list.
+            result.append("REDIRECT")
+            result.append("views.question")
+            result.append("getQuestion")
+            result.append(question_id)
+            return result
+        # If question already has a best answer the
+        else:
+            # Return error message to user
+            # TODO: Log error to file instead.
+            return "ACCEPTED_ANSWER_EXISTS"
--- services/web/project/app/views.py
+++ services/web/project/app/views.py
@@ -4,7 +4,7 @@ from flask.helpers import send_file
 from flask_login import login_required, current_user
 from project import db
 from project.app.models import User, Question, Answer
-from project.app.forms import NewQuestionForm, NewAnswerForm
+from project.app.forms import NewQuestionForm
 from project.app.controllers import QuestionCtrl
 
 views = Blueprint('views', __name__)
@@ -19,11 +19,15 @@ def home():
 def questions():
     # Create a list of questions.
     questions = Question.query.order_by(Question.date_created).all()
-    # For each question add the username of the creator, and the number of answers as attributes.
+    # For each question add the username of the creator,
+    # and the number of answers as attributes.
     for q in questions:
         q.creator = User.query.get(q.userId)
         q.numAnswers = len(Answer.query.filter_by(questionId=q.id).all())
-    return render_template("questions.html", user=current_user, questions_list=questions)
+    return render_template(
+        "questions.html",
+        user=current_user,
+        questions_list=questions)
 
 
 @views.route('/question', methods=['GET', 'POST'])
@@ -37,32 +41,51 @@ def new_question():
         q = Question(title, body, current_user.id)
         db.session.add(q)
         db.session.commit()
-        return redirect(url_for("views.question", method="getQuestion", arg1=q.id))
+        return redirect(url_for(
+            "views.question",
+            method="getQuestion",
+            arg1=q.id))
 
     return render_template("new_question.html", form=form)
 
 
-@views.route('/questions/', defaults={'method': 'None', 'arg1': 'None', 'arg2': 'None'})
-@views.route('/questions/<method>/', defaults={'arg1': 'None', 'arg2': 'None'})
-@views.route('/questions/<method>/<arg1>', defaults={'arg2': 'None'}, methods=['GET', 'POST'])
+@views.route('/questions/', defaults={
+    'method': 'None',
+    'arg1': 'None',
+    'arg2': 'None'})
+@views.route('/questions/<method>/', defaults={
+    'arg1': 'None',
+    'arg2': 'None'})
+@views.route('/questions/<method>/<arg1>', defaults={
+    'arg2': 'None'},
+    methods=['GET', 'POST'])
 @views.route('/questions/<method>/<arg1>/<arg2>', methods=['GET', 'POST'])
 def question(method, arg1, arg2):
     # Make best answer method
     if(method == "acceptAnswer"):
         result = QuestionCtrl.acceptAnswer(arg1, arg2)
         if(result[0] == "REDIRECT"):
-            return redirect(url_for(result[1], method=result[2], arg1=result[3]))
+            return redirect(url_for(
+                result[1],
+                method=result[2],
+                arg1=result[3]))
         else:
-            return result    
+            return result
     # Get Queston method
     elif(method == "getQuestion"):
         result = QuestionCtrl.getQuestion(arg1)
         if(result[0] == "REDIRECT_1"):
             return redirect(url_for(result[1]))
         elif(result[0] == "REDIRECT_2"):
-            return redirect(url_for(result[1], method=result[2], arg1=result[3]))
+            return redirect(url_for(
+                result[1],
+                method=result[2],
+                arg1=result[3]))
         else:
-            return render_template(result[1], question=result[2], form=result[3])
+            return render_template(
+                result[1],
+                question=result[2],
+                form=result[3])
     else:
         return redirect(url_for("views.questions"))
 
