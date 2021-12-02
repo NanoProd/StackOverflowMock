821933faf2ed3687b166095573b4e6b869e150cf
daxsis <vinokhodovalex@gmail.com>
2021-12-01 23:22:51 -0500

    add test error handler for 404

--- services/web/project/main.py
+++ services/web/project/main.py
@@ -1,6 +1,15 @@
+from flask.templating import render_template
 from project import create_app
 
+
 app = create_app()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
